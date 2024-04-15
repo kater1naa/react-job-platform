096d396289b9b771fea1ab43c8b54182c57ec3d0
Katerina Mastrogianni <katerinamastrog@gmail.com>
2024-04-15 22:31:15 +0200

    did ViewAllJobs component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      <HomeCards></HomeCards>
-      <JobListings></JobListings>
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
   );
 };
