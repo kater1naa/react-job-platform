9de26ac92905c195399613196fc1c237787566ac
Katerina Mastrogianni <katerinamastrog@gmail.com>
2024-04-15 22:21:21 +0200

    did JobListings component and child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <HomeCards></HomeCards>
+      <JobListings></JobListings>
     </>
   );
 };
